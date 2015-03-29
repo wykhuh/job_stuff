d05c6a352559c23d420b0a274e15eb24ed69ef01
wykhuh <wykhuh@users.noreply.github.com>
2015-03-29 12:37:02 -0700

    refactor

--- TweetSec.rb
+++ TweetSec.rb
@@ -3,57 +3,82 @@ require './words'
 
 class TweetSec 
 
-    attr_accessor :original_tweet, :modified_tweet
+    attr_accessor :original_password, :modified_password
 
-    # capture tweet sent to account
     def initialize(tweet)
-        @original_tweet = tweet
-        @modified_tweet = @original_tweet
+        @original_password = tweet
+        @modified_password = tweet
+        @letters_arr = ('a'..'z').to_a + ('A'..'Z').to_a
+        @digits_arr = (0..9).to_a
+        @other_arr = ['#','%','^','*','<','>','/','-','+','~']
+        @strong_password_min = 50
+        @weak_password_min = 10
+        # 50  / 4 types = 12.5; with 4 types, 13 chars is shortest allowable password
+        @password_length_min = (@strong_password_min.to_f / 4).ceil
     end
 
-    #find words in tweet
-    def find_words 
-        # find letter combos greater than two letters
-        @original_tweet.scan(/[A-Za-z]{2,}/) do |word|
-            if (validate_word(word)) 
-                replace_word(word)
+    def check_submitted_password
+        # find all letter combos
+        combos = find_combos
+
+        # if combo is a word, replace valid word with letter
+        combos.each do |combo|
+            if (validate_combo(combo)) 
+                replace_word(combo)
             end
         end
+
+        # calculate the score of the modified password
+        score = calculate_score(@modified_password)
+
+        # evaluate password and show response to user
+        evaluate_password(score)
+
     end
 
-    #check if the word is in the $word_list
-    def validate_word(word)
-        $word_list[word] ? true: false
+    #find letter combos in password
+    def find_combos 
+        combos = []
+        # find letter combos greater than two letters
+        @original_password.scan(/[A-Za-z]{2,}/) do |combo|
+            combos << combo
+        end
+        combos
+    end
+
+    #check if the combo is in the $word_list
+    def validate_combo(combo)
+        $word_list[combo] ? true: false
     end
 
  
     #replace words with one letter
     def replace_word(word)
-        letter = word[0]
+        # pick random letter from word
+        letter = word[rand(0..word.length-1)]
 
-        # replace word if word is in the middle of the tweet
-        @modified_tweet = @modified_tweet.gsub(/([^A-Za-z])#{word}([^A-Za-z])/, '\1'+letter+'\2')
-        # replace word if word is at the start of tweet
-        @modified_tweet = @modified_tweet.gsub(/\b#{word}([^A-Za-z])/, letter+'\1')
-        # replace word if word is at end of tweet
-        @modified_tweet = @modified_tweet.gsub(/([^A-Za-z])#{word}\b/, '\1'+letter)
-        # replace word if word is the entire tweet
-        @modified_tweet = @modified_tweet.gsub(/\b#{word}\b/, letter)
+        # replace word if word is in the middle of the password
+        @modified_password = @modified_password.gsub(/([^A-Za-z])#{word}([^A-Za-z])/, '\1'+letter+'\2')
+        # replace word if word is at the start of password
+        @modified_password = @modified_password.gsub(/\b#{word}([^A-Za-z])/, letter+'\1')
+        # replace word if word is at end of password
+        @modified_password = @modified_password.gsub(/([^A-Za-z])#{word}\b/, '\1'+letter)
+        # replace word if word is the entire password
+        @modified_password = @modified_password.gsub(/\b#{word}\b/, letter)
 
-        puts @modified_tweet
     end 
 
     #calculate score
-    def calculate_score
+    def calculate_score(password)
         @counts = { :letter => 0,
             :digit => 0,
             :whitespace => 0,
             :other => 0 }
 
-        @score = 0
+        score = 0
 
         # iterate through each character, and count each type
-        @modified_tweet.each_byte do |c|
+        password.each_byte do |c|
             #letters (upper or lower)
             if (c >=65 and c <= 90) or (c >=97 and c <= 122)
                 @counts[:letter] += 1
@@ -72,40 +97,45 @@ class TweetSec
         #find types whose counts > 0
         existing_types = @counts.select { |type|  @counts[type] > 0}
 
-        @score =  existing_types.length * @modified_tweet.length
-        puts @score
+        score =  existing_types.length * password.length
     end
 
-    # show response
-    def show_response
-        # strong password : congrats
-        if @score >= 50
-            puts 'Congrats. Password is strong'
-        # weak password : modify tweet
-        elsif  @score > 10
-            strengthen_weak_password
-            puts 'Password is weak. Here is a better password:', @modified_tweet
+    # evaluate password and show response to user
+    def evaluate_password(score)
 
-        # unacceptable : reject tweet
+        # strong password : congrats
+        if score >= @strong_password_min
+            puts 'Congrats. Password is strong'
+
+        # weak password : modify password
+        elsif  score > @weak_password_min
+            strengthen_weak_password
+            score = calculate_score(@modified_password)
+            puts 'Password is weak. Here is a better password:', @modified_password
+
+        # unacceptable : reject password
         else
             puts 'Enter stronger password.'
         end
-        puts 'score: ' + @score.to_s
+
+        puts 'score: ' + score.to_s
     end
 
-    # strengthen a weak tweet
-    # 13 characters * 4 types = 52; 13 and 4 types is shortest allowable tweet
+    # strengthen a weak password by adding types and/or characters
     def strengthen_weak_password
 
-        # if tweet length >= 13, modify characters to eliminate missing types
-        if @modified_tweet.length >= 13
+        # if password length >= minimum length, modify characters to eliminate missing types
+        if @modified_password.length >= @password_length_min
             replace_characters
 
-        # else if length < 13, lengthen tweet and modify characters
+        # else if password length < minimum length, lengthen password and modify characters
         else
-            # add characters to tweet until tweet is 13 characters
-            while(@modified_tweet.length < 13)
-                @modified_tweet = ('a'..'z').to_a[rand(0..25)] + @modified_tweet
+            # add random character to beginning of password until password is minimum length
+            while(@modified_password.length < @password_length_min)
+                all_arr = @letters_arr + @digits_arr + @other_arr + [' ']
+                # convert to string because there are numbers in all_arr
+                random_character = all_arr[rand(0..all_arr.length-1)].to_s
+                @modified_password =   random_character + @modified_password
             end
             # modify characters to eliminate missing types
             replace_characters
@@ -116,59 +146,47 @@ class TweetSec
     # we only modify characters whose type count > 1 so that we don't
     # eliminate an existing type.
     def get_modifiable_types
-        modifiable_types = @counts.select { |type|  @counts[type] > 1}
-        puts modifiable_types
-        modifiable_types
+        @counts.select { |type|  @counts[type] > 1}
     end
 
     def get_missing_types
-        missing_types  = @counts.select { |type|  @counts[type] == 0}
-        puts missing_types
-        missing_types
+        @counts.select { |type|  @counts[type] == 0}
     end
 
-    # replace characters in the tweet
+    # replace characters in the password
     def replace_characters
-        # for each type, list the regex and one random character  
+        # each type has regex and one random character  
         types_data = { 
             :letter => {'regex' => '[A-Za-z]',
-                'replace' => ('a'..'z').to_a[rand(0..25)] }, 
+                'replace' => @letters_arr[rand(0..51)] }, 
             :digit => {'regex' => '[0-9]',
-                'replace' => (0..9).to_a[rand(0..9)].to_s },
+                'replace' => @digits_arr[rand(0..9)].to_s },
             :whitespace => {'regex' => '[\t\n ]',
                 'replace' => ' ' },
             :other => {'regex' => '[^A-Za-z0-9\t\n ]',
-                'replace' => ['#','%','^','*','<','>','/','-','+','~'][rand(0..9)] }
+                'replace' => @other_arr[rand(0..9)] }
         }
 
-        # get all types whose count > 0, thereby are missing
+        # get all missing types 
         missing_types = get_missing_types
 
         missing_types.each do |missing_type|
-            # get first type whose count > 1, thereby is modifiable
+            # get first type that is modifiable
             first_modifiable_type = get_modifiable_types.keys[0]
 
-            # search the tweet and replace a character from modifiable_types
+            # search the password and replace a character from modifiable_types
             # with a character from missing_types
             regex = types_data[first_modifiable_type]['regex']
             replacement = types_data[missing_type[0]]['replace']
 
-            @modified_tweet = @modified_tweet.sub(/#{regex}/, replacement )
-            puts @modified_tweet
-            calculate_score
-
+            @modified_password = @modified_password.sub(/#{regex}/, replacement )
         end
 
     end
 
 end
 
-my_tweet = TweetSec.new(ARGV[0])
+my_password = TweetSec.new(ARGV[0])
 
-#puts my_tweet.original_tweet
-
-
-my_tweet.find_words
-my_tweet.calculate_score
-my_tweet.show_response
+my_password.check_submitted_password
 
